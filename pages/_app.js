a232010bcb4b8d7d8a09d5ab4c48f1c7137e3b21
Emily Yu <bluerainmango@gmail.com>
2020-08-05 20:22:51 -0700

    created _app.js and added polaris to index.js

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import App from "next/app";
+import Head from "next/head";
+// AppProvider : app을 감싸 polaris가 전체 앱에 적용되도록 하는 태그.
+import { AppProvider } from "@shopify/polaris";
+import "@shopify/polaris/styles.css";
+// translations: 언어 지원
+import translations from "@shopify/polaris/locales/en.json";
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Emily's app</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <AppProvider i18n={translations}>
+          <Component {...pageProps} />
+        </AppProvider>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-const Index = () => (
-  <div>
-    <p>Sample app using React and Next.js</p>
-  </div>
-);
+import { EmptyState, Layout, Page } from "@shopify/polaris";
+
+function Index() {
+  return (
+    <Page>
+      <Layout>
+        <EmptyState
+          heading="Manage your inventory transfers"
+          action={{ content: "Add transfer" }}
+          secondaryAction={{
+            content: "Learn more",
+            url: "https://help.shopify.com",
+          }}
+          image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+        >
+          <p>Emily's first page</p>
+        </EmptyState>
+      </Layout>
+    </Page>
+  );
+}
 
 export default Index;
