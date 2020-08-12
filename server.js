53ae3de891740b8bef013a03c269a00ebb7817b5
Emily Yu <bluerainmango@gmail.com>
2020-08-11 22:11:18 -0700

    added graphiQL proxy to server

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const { verifyRequest } = require("@shopify/koa-shopify-auth");
 const session = require("koa-session");
 
 dotenv.config();
+
+const { default: graphQLProxy } = require("@shopify/koa-shopify-graphql-proxy");
+const { ApiVersion } = require("@shopify/koa-shopify-graphql-proxy");
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -39,6 +43,8 @@ app.prepare().then(() => {
       },
     })
   );
+
+  server.use(graphQLProxy({ version: ApiVersion.October19 }));
 
   server.use(verifyRequest());
 
