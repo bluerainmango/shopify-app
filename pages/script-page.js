3660f63270d1a9d6947b2d215c8072247ba267b0
Emily Yu <bluerainmango@gmail.com>
2020-08-22 00:25:46 -0700

    fixed a bug: spelling error led to broken page - items property at ResourceList tag

--- pages/script-page.js
+++ pages/script-page.js
@@ -93,7 +93,7 @@ function ScriptPage() {
             <ResourceList
               showHeader
               resourceName={{ singular: "Script", plural: "Scripts" }}
-              item={data.scriptTags.edges}
+              items={data.scriptTags.edges}
               renderItem={(item) => {
                 return (
                   <ResourceList.Item id={item.id}>
@@ -129,46 +129,3 @@ function ScriptPage() {
 }
 
 export default ScriptPage;
-
-//! ScriptPage 의 return 으로 들어가있던 값. 너무 ugly해서 삭제함.
-// {/* <div>
-//   {console.log("data:", data)}
-//   <h1>These are the script tags right now:</h1>
-//   {/* scriptag를 생성하는 버튼. refetchQueries 옵션은 scripttag 생성 후, 자동으로 재 query실행해 최신 데이터를 출력 */}
-//   <button
-//     type="submit"
-//     onClick={() => {
-//       createScripts({
-//         variables: {
-//           input: {
-//             src: "https://e14ce1d77593.ngrok.io/test-script.js",
-//             displayScope: "ALL",
-//           },
-//         },
-//         refetchQueries: [{ query: QUERY_SCRIPTTAGS }],
-//       });
-//     }}
-//   >
-//     Create Script Tag
-//   </button>
-//   {data.scriptTags.edges.map((item) => {
-//     return (
-//       <div key={item.node.id}>
-//         <p>{item.node.id}</p>
-//         <button
-//           type="submit"
-//           onClick={() => {
-//             deleteScripts({
-//               variables: {
-//                 id: item.node.id,
-//               },
-//               refetchQueries: [{ query: QUERY_SCRIPTTAGS }],
-//             });
-//           }}
-//         >
-//           Delete Script Tag
-//         </button>
-//       </div>
-//     );
-//   })}
-// </div> */}
